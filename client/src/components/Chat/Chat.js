88dccb56cfa8c72e8122f495d2451143b22cf8ab
Shivanshu981 <developer.shivanshu@gmail.com>
2020-04-29 07:31:32 -0700

    ENDPOINT Changed in Chat.js as per Heroku Server

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -17,7 +17,7 @@ const Chat = ({ location }) => {
   const [room, setRoom] = useState('');
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
-  const ENDPOINT = 'localhost:5000';
+  const ENDPOINT = 'https://evenchat.herokuapp.com/';
 
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
